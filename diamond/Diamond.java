54dff079259b44391cb708fba726a6fe4cf1d813
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2026-04-02 16:41:27 -0400

    added the scanner and assigned the input

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int n = scan.nextInt();
+    if (n <= 0) {
+      System.out.println("Number must be positive !")
+    }
+    scan.close();
   }
 }
