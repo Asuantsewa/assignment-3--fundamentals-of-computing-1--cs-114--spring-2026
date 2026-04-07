377b6a548ad68ca59baa7cd071944a9578d75045
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2026-04-07 11:46:42 -0400

    odd diamond

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -3,9 +3,31 @@ public class Diamond {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     System.out.println("Enter a number: ");
-    int n = scan.nextInt();
-    if (n <= 0) {
-      System.out.println("Number must be positive !")
+    int size = scan.nextInt();
+    if (size <= 0) {
+      System.out.println("Number must be positive !");
+      return;
+    }
+    if (size % 2 == 0) {
+    }
+
+
+
+    // odd diamond
+    public static void OddDiamond(int n) {
+      int mid = (size/2);
+
+      for (int i = 0; i <n; i++){
+        int stars = i <= mid ? (2 * i + 1) : (2*(n -1 - i) + 1);
+        int spaces = Math.abs(mid - i);
+
+        for (int s = 0; s < spaces; s++){
+          System.out.print(" ");
+        }
+        for (int st = 0; s < stars; st++);
+        Sysytem.out.print("*");
+      }
+      System.out.println();
     }
     scan.close();
   }
